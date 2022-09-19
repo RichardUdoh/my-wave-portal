fa054aa3b78d0a6d36283be41f42400baa06d3bf
Richard Udoh <udohrichard201114@gmail.com>
2022-09-19 15:16:11 +0000

    writting first smart contract syntax

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log(
+            "Yo yo this is Richi on the keyboard, I am a contract and I am smart"
+        );
+    }
+}
